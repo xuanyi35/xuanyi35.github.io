6f34638ddf8fe952359233e5b011be7425083222
xuanyi35 <31713273+xuanyi35@users.noreply.github.com>
2018-08-24 14:27:23 -0600

    Web

--- script/demoscript.js
+++ script/demoscript.js
@@ -5,6 +5,31 @@ demo = document.getElementById("demo");
 if  (url.includes("Website")){
      board.style.display = "none";
      demo.style.display = "block";
+	
+    Pname = document.getElementById("Pname");
+    Pname.innerText = "Movie Station";
+    subname = document.getElementById("subname");
+    subname.innerText = "Solo ASP.NET CORE MVC Web Project";
+    
+    description = document.getElementById("desc");
+    description.innerHTML = ' Solo project for practice. <br/> HTML5, CSS, Javascript & c#, ASP.NET CORE 2.0, Entity Framework Core & MSSQL, LiINQ. <br/> This website is used to Explore Movies. Features: <br/><br/> 1. Show Latest Movies<br/> 2. Search Movie by Keyword/Genre <br/> 3. View Movie details <BR/> 4. Recommend Similar Movies<br/> 5. View Trailer <br/> 6. Add to your Favourite Movie List <br/>7. Login/Sign up to organize your favourtie movie list</br/>8. Wait for you to Explore More!';
+    
+    ui = document.getElementById("ui");
+    ui. src = "../img/Movie_UI.png";
+    ui.onclick = function () {
+		window.open("../img/Movie_UI.png");
+    } 
+     
+    /*var link = "https://www.youtube.com/embed/LeQVUgIUshQ";*/
+    video = document.getElementById("video");
+   /* video. src = link;*/
+    video.style.display = "none";
+    document.getElementById("videoT").style.display = "none";
+	
+    github = document.getElementById("git");
+    github.href = "https://github.com/xuanyi35/MvcMovie1" ;
+    document.getElementById("web").href = "https://xuanyi-movie.gear.host/"; 	
+	
 }
 else if (url.includes("Mobile")){
     board.style.display = "none";
